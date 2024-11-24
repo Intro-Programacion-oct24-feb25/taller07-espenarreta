11173f605e73bc0449bcb734b73c437154d3c939
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-11-23 23:55:48 -0500

    Problemas resueltos

--- problematicas/Problema04/src/problema04/Problema04.java
+++ problematicas/Problema04/src/problema04/Problema04.java
@@ -14,7 +14,31 @@ public class Problema04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here        
+        double resultado = 0;
+        String secuencia = "1";
+        int signo = -1;
+        int dem = 3;
+        
+        System.out.println("Serie: ");
+
+        while (dem <= 15) {
+            if (signo == 1) {
+                secuencia = secuencia + " + 1/" + dem;
+            } else {
+                secuencia = secuencia + " - 1/" + dem;
+            }
+
+            resultado = resultado + signo * (1.0 / dem);
+
+            signo = signo * -1;
+
+            dem = dem + 2;
+        }
+
+        resultado =  resultado + 1;
+        
+        System.out.println(secuencia);
+        System.out.println("Resultado de la serie: " + resultado);
     }
-    
 }
